b4ac7953d0f1cda50c9663a1860dc06378c67c52
Iris M <iris.mehrbrodt@gmx.de>
2015-01-18 21:50:41 +0100

    init

---
+++ MultiPong/src/de/pinkproblem/multipong/MainMenu.java
@@ -0,0 +1,13 @@
+package de.pinkproblem.multipong;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainMenu extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_menu);
+	}
+}
